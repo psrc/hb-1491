d8711fe4f33fc81ece9f3bf2d66a79d15b043a89
stefancoe <coestefan@gmail.com>
2025-07-05 09:33:54 -0700

    Changes to script.

--- hb_1491/src/capacity_analysis_1491.py
+++ hb_1491/src/capacity_analysis_1491.py
@@ -1,15 +1,12 @@
-import pyogrio
-import pymysql.cursors
-import pymysql
 import numpy as np
 from pathlib import Path
-import os
 import pandas as pd
 import geopandas as gpd
-from pathlib import Path
 
 
 def buffer_stops(stops, crs):
+    """Buffers the stops GeoDataFrame by the 'buffer_size' column."""
+
     dfs = []
     buffer_sizes = list(stops["buffer_size"].unique())
     for buffer_size in buffer_sizes:
@@ -20,35 +17,221 @@ def buffer_stops(stops, crs):
     return gpd.GeoDataFrame(merged, geometry="geometry", crs=crs)
 
 
-def run(config):
-    # h5_cache_dir = r'N:\base_year_2023_inputs\urbansim2_inputs\psrc_base_year_2023_alloc_py3.h5'
-    # gis_data = Path(r'T:\60day-TEMP\Stefan\HB_transit.gdb')
-    hb_1110_lookup = pd.read_csv(Path(config["data_dir"]) / "hb_1110_lookup.csv")
+def group_by_sum_indices(arr, x):
+    """Groups indices of an array such that the sum of the values in each group is at least x."""
+    orig_indices = []
+    group_indices = []
+    group_sum = 0
+    group_id = 0
+    for idx, num in enumerate(arr):
+        orig_indices.append(idx)
+        group_indices.append(group_id)
+        group_sum += num
+        if group_sum >= x:
+            group_id += 1
+            group_sum = 0
+    return orig_indices, group_indices
 
-    plan_type_FAR = pd.read_csv(Path(config["data_dir"]) / "plan_type_FAR_lookup.csv")
+
+def find_closest_matches(list1, list2):
+    """
+    Matches each value in list1 to the closest value in list2.
+
+    Args:
+        list1 (list): The list of values to match.
+        list2 (list): The list of values to find matches within.
+
+    Returns:
+        list: A list containing the closest matches.
+    """
+    matches = []
+    for val1 in list1:
+        closest_val2 = min(list2, key=lambda x: abs(x - val1))
+        matches.append(closest_val2)
+    return matches
+
+
+def average_built_sqft_per_unit(
+    buildings, parcels, max_du_acre=1400, min_group_size=50
+):
+    """Calculates the average built square footage per unit for multifamily
+    buildings for groupings of dwelling units per acre."""
+
+    # get multifamily buildings & apply filters
+    buildings = buildings[buildings["building_type_id"].isin([4, 10, 12])]
+    buildings = buildings[buildings["residential_units"] > 0]
+    buildings = buildings[buildings["sqft_per_unit"] > 0]
+    buildings["res_sqft"] = buildings["residential_units"] * buildings["sqft_per_unit"]
+    buildings["buildings_count"] = 1
+
+    # aggregate buildings by parcel_id
+    buildings = (
+        buildings.groupby("parcel_id")
+        .agg({"res_sqft": "sum", "residential_units": "sum", "buildings_count": "sum"})
+        .reset_index()
+    )
+    # merge buildings with parcels
+    parcels = parcels.merge(buildings, on="parcel_id", how="inner")
+
+    # further filtering
+    parcels = parcels[parcels["residential_units"] > 1]
+    parcels = parcels[parcels["res_sqft"] > 0]
+    parcels["sqft_per_unit"] = parcels["res_sqft"] / parcels["residential_units"]
+    parcels = parcels[parcels["sqft_per_unit"] < 5000]
+    parcels = parcels[parcels["sqft_per_unit"] > 100]
+    parcels = parcels[parcels["parcel_sqft"] > 0]
+
+    parcels["as_built_du_acre"] = parcels.residential_units / (
+        parcels.parcel_sqft / 43560
+    )
+
+    # round the as_built_du_acre to the nearest 10 to create groupings
+    parcels["as_built_du_acre_rounded"] = (parcels.as_built_du_acre / 10).round() * 10
+    freq = parcels["as_built_du_acre_rounded"].value_counts().reset_index()
+
+    # find groups that have less than min_group_size
+    small_sample = freq[freq["count"] < min_group_size].reset_index()
+
+    # create groups that have at least min_group_size
+    index, groupings = group_by_sum_indices(small_sample["count"].values, 50)
+    small_sample["groupings"] = groupings
+    small_sample["du_acre_final"] = small_sample.groupby("groupings")[
+        "as_built_du_acre_rounded"
+    ].transform("max")
+
+    # merge the small sample with parcels, include 'du_per_acre_final' column,
+    # which is used to join plan types outside this function
+    parcels = parcels.merge(
+        small_sample[["as_built_du_acre_rounded", "du_acre_final"]],
+        on="as_built_du_acre_rounded",
+        how="left",
+    )
+    parcels["du_acre_final"].fillna(parcels["as_built_du_acre_rounded"], inplace=True)
+    parcels.loc[parcels["du_acre_final"] > max_du_acre, "du_acre_final"] = max_du_acre
+
+    built_sqft_per_unit = (
+        parcels.groupby("du_acre_final")
+        .agg({"res_sqft": "sum", "residential_units": "sum", "buildings_count": "sum"})
+        .reset_index()
+    )
+    built_sqft_per_unit["sqft_feet_per_unit"] = (
+        built_sqft_per_unit["res_sqft"] / built_sqft_per_unit["residential_units"]
+    )
+
+    return built_sqft_per_unit
+
+
+def create_plantype_FAR_lookup(plan_types):
+    """Creates a (flattened) lookup table for each plan types with min and max DU (SF & MF) & FAR (MU)."""
+
+    # rename constrant_type 'units_per_acre' to 'du' for consistency
+    plan_types.loc[
+        plan_types["constraint_type"] == "units_per_acre", "constraint_type"
+    ] = "du"
+
+    plan_types.rename(
+        columns={
+            "minimum": "min",
+            "maximum": "max",
+        },
+        inplace=True,
+    )
+
+    plan_types["plan_label"] = (
+        plan_types.generic_land_use_type_id.astype(str)
+        + "_"
+        + plan_types.constraint_type.astype(str)
+    )
+
+    df = (
+        plan_types.groupby(["plan_type_id", "plan_label"])
+        .agg({"min": "max", "max": "max"})
+        .reset_index()
+    )
+
+    df["idx"] = df.groupby("plan_type_id").cumcount() + 1
+    df = df.pivot_table(
+        index=["plan_type_id"],
+        columns="plan_label",
+        values=["min", "max"],
+        aggfunc="first",
+    )
+
+    df = df.sort_index(axis=1, level=1)
+    df.columns = [f"{x}_{y}" for x, y in df.columns]
+    df = df.reset_index()
+    df.fillna(0, inplace=True)
+    df["max_du"] = df[["max_1_du", "max_2_du", "max_6_du"]].max(axis=1)
+    df["max_far_mixed_use"] = df["max_6_far"]
+    return df
+
+def percent_parcels_meet_far(parcels, far_threshold):
+    """Calculates the percentage of parcels that meet the FAR threshold."""
+    parcels = parcels[parcels["final_far"] > 0]
+    parcels["meets_far_threshold"] = parcels["final_far"] >= far_threshold
+    percent_meeting_far = (
+        parcels["meets_far_threshold"].sum() / len(parcels) * 100
+    )
+    return percent_meeting_far
+
+def run(config):
+    # has FAR assumptions for single family & middle houseing
+    hb_1110_lookup = pd.read_csv(Path(config["data_dir"]) / "hb_1110_lookup.csv")
+    store = pd.HDFStore(config["h5_cache_dir"], mode="r")
+
+    # parcels
+    parcels = store["parcels"]
+    parcels.reset_index(inplace=True)
+
+    # builduings
+    buildings = store["buildings"]
+    buildings.reset_index(inplace=True)
+
+    # plan types
+    plan_types = store["development_constraints"]
+
+    # walksheds have been created for the 1491 stops
     if config["use_walksheds"]:
         stops_gdf = gpd.read_file(config["output_gdb"], layer="stations_1491_walksheds")
     else:
         stops_gdf = gpd.read_file(config["output_gdb"], layer="stations_1491")
 
-    # stops_gdf = stops_gdf.groupby('stop_name').first().reset_index()
     stops_gdf = stops_gdf[
         ["stop_name", "new_id", "geometry", "stop_type", "buffer_size"]
     ]
-    stops_gdf = stops_gdf[stops_gdf["stop_name"] != "Untitled Stop"]
+
+    # stops_gdf = stops_gdf[stops_gdf["stop_name"] != "Untitled Stop"]
+
+    # buffer stops if not using walksheds
     if not config["use_walksheds"]:
         stops_gdf = buffer_stops(stops_gdf, stops_gdf.crs)
-    # stops_gdf = stops_gdf.dissolve(by='stop_name', as_index=False)
 
-    # stops_gdf['geometry'] = stops_gdf.geometry.buffer(2640)
-
-    store = pd.HDFStore(config["h5_cache_dir"], mode="r")
-    parcels = store["parcels"]  # extract the households dataset for the 2023 base year
-    parcels.reset_index(inplace=True)  # reset index to avoid issues with merging
-    # plan types
-    plan_types = store[
-        "development_constraints"
-    ]  # extract the buildings dataset for 2050
+    # get the built sqft per unit for ranges of dwelling units per acre
+    built_sqft_by_units_acre = average_built_sqft_per_unit(buildings, parcels)
+    
+    # create a lookup table for plan types with max DU and FAR
+    plan_type_FAR = create_plantype_FAR_lookup(plan_types)
+    
+    # create a crosswalk between plan types and built sqft per unit
+    plan_type_FAR["du_acre_lookup"] = find_closest_matches(
+        plan_type_FAR["max_du"].tolist(),
+        built_sqft_by_units_acre["du_acre_final"].tolist(),
+    )
+    # merge the built sqft per unit with plan types
+    plan_type_FAR = plan_type_FAR.merge(
+        built_sqft_by_units_acre[["du_acre_final", "sqft_feet_per_unit"]],
+        left_on="du_acre_lookup",
+        right_on="du_acre_final",
+        how="left",
+    )
+    # calculate max FAR for residential parcels
+    plan_type_FAR["max_far_res"] = (
+        plan_type_FAR["max_du"] * plan_type_FAR["sqft_feet_per_unit"] / 43560
+    )
+    # calculate max FAR for plan type
+    plan_type_FAR["max_res_far_current_zoning"] = plan_type_FAR[
+        ["max_far_res", "max_far_mixed_use"]
+    ].max(axis=1)
 
     # get generic land use types
     res_plan_types = plan_types[
@@ -59,16 +242,16 @@ def run(config):
         .max()
         .reset_index()
     )
-    parcels = parcels.merge(
-        res_plan_types[["plan_type_id", "generic_land_use_type_id"]],
-        on="plan_type_id",
-        how="left",
-    )
+    # parcels = parcels.merge(
+    #     res_plan_types[["plan_type_id", "generic_land_use_type_id"]],
+    #     on="plan_type_id",
+    #     how="left",
+    # )
 
-    # only need residential parcels
-    parcels = parcels[parcels["generic_land_use_type_id"].isin([2, 1, 6])]
+    # # only need residential parcels
+    # parcels = parcels[parcels["generic_land_use_type_id"].isin([2, 1, 6])]
 
-    # create dummy for parcesl greater than 10k sqft
+    # create dummy for parcels greater than 10k sqft
     parcels["10k_sqft_plus"] = (parcels["parcel_sqft"] > 10000).astype(int)
 
     # merge hb_1110_lookup to parcels for
@@ -76,29 +259,21 @@ def run(config):
         hb_1110_lookup, on=["hb_tier", "hb_hct_buffer", "10k_sqft_plus"], how="left"
     )
 
-    # get max FAR by plan type
+    # join plan types with parcels
     parcels = parcels.merge(plan_type_FAR, on="plan_type_id", how="left")
-    # far_plan_types = plan_types[plan_types.constraint_type == 'far'] # filter for FAR constraints
-    # far_plan_types = far_plan_types.groupby('plan_type_id').max().reset_index()
-    # parcels = parcels.merge(far_plan_types[['plan_type_id', 'maximum']], on='plan_type_id', how='left')
-    # parcels = parcels.rename(columns={'maximum': 'far_maximum'})
-    # # set FAR to 0 for non-mixed use parcels
-    # parcels['far_maximum'] = np.where(parcels['generic_land_use_type_id'] == 6, parcels['far_maximum'], 0)
 
-    # # get max DU per acre by plan type
-    # du_plan_types = plan_types[plan_types.constraint_type == 'units_per_acre'] # filter for DU constraints
-    # du_plan_types = du_plan_types.groupby('plan_type_id').max().reset_index()
-    # parcels = parcels.merge(du_plan_types[['plan_type_id', 'maximum']], on='plan_type_id', how='left')
-    # parcels = parcels.rename(columns={'maximum': 'du_maximum'})
-    # parcels = parcels.merge(du_to_far, left_on='du_maximum', right_on='zoned_max_du_acre', how='left')
-    # #parcels['du_maximum_far'] = parcels['du_maximum'] * 0.0157 + 0.2283
+    # keep only parcels that have far greater than 0
+    parcels = parcels[parcels['max_res_far_current_zoning']>0]
+    
+    # only apply hb_res_far to parcels that are not mixed use or mf:
+    parcels["hb_res_far"] = np.where(
+        parcels["max_du"] > 12, 0, parcels["hb_res_far"]
+    )
 
     parcels = parcels.fillna(0)
     parcels["max_res_far_current_zoning"] = parcels.max_res_far_current_zoning.astype(
         float
     )
-
-    # plan_types = plan_types.groupby('plan_type_id').agg({'maximum': 'max'}).reset_index()
 
     parcels["final_far"] = parcels[["max_res_far_current_zoning", "hb_res_far"]].max(
         axis=1
@@ -120,9 +295,9 @@ def run(config):
 
     data = []
     df_list = []
-    # station_area_parcels = pd.GeoDataFrame(columns=[parcels_gdf.columns + ['stop_name']])
+    # iterate through each buffer and calculate the weighted FAR
     for buffer in stops_gdf.iterrows():
-        buffer = buffer[1]  # Get the row data
+        buffer = buffer[1]
         parcels_in_buffer = parcels_gdf[parcels_gdf.geometry.within(buffer.geometry)]
         percent_weights_by_land_use = (
             parcels_in_buffer.groupby("final_land_use")["final_far_weight"].sum()
@@ -143,6 +318,11 @@ def run(config):
             percent_weights_by_mixed_use = percent_weights_by_land_use[6]
         else:
             percent_weights_by_mixed_use = 0
+        
+        if buffer.stop_type == 5:
+            percent_meets = percent_parcels_meet_far(parcels_in_buffer, 2.5)
+        else:
+            percent_meets = percent_parcels_meet_far(parcels_in_buffer, 3.5)
         data.append(
             {
                 "stop_name": buffer.stop_name,
@@ -152,6 +332,7 @@ def run(config):
                 "percent_weights_by_sf": percent_weights_by_sf,
                 "percent_weights_by_mf": percent_weights_by_mf,
                 "percent_weights_by_mixed_use": percent_weights_by_mixed_use,
+                "percent_meets_far": percent_meets,
             }
         )
         parcels_in_buffer["stop_name"] = buffer.stop_name
